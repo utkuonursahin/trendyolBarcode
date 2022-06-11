53f54384d0f2cfb81ccceb70c214ec9f1f5a4732
utkuonursahin <sahinutkuonur@gmail.com>
2022-06-11 11:02:15 +0300

    dev

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const barcode = window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.variants[0].barcode
-const barcodeEl = document.querySelector('.barcode')
+// const barcode = window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.variants[0].barcode
+// const barcodeEl = document.querySelector('.barcode')
+
+function codeToInject() {
+    console.log(window.__PRODUCT_DETAIL_APP_INITIAL_STATE__)
+}
+
+const inject = function (fn) {
+    const script = document.createElement("script");
+    script.text = `(${fn.toString()})();`;
+    document.documentElement.appendChild(script)
+    console.log(window.__PRODUCT_DETAIL_APP_INITIAL_STATE__)
+}
+inject(codeToInject)
